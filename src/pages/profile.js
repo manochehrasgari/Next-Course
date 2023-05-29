b816e5d1f7ecccc9c673a7aef11e423ce4698dfa
Manochehr Asgari <manochehrasgari@yahoo.com>
2023-05-29 07:28:17 -0800

    finish profile user >>  clinet side authentication part2

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,9 +1,25 @@
 import Layout from "@/containers/Layout/Layout";
+import { signIn, useSession } from "next-auth/react";
 
 const Profile = () => {
+
+
+    const { data:session, status } = useSession({
+        required: true,
+        onUnauthenticated() {
+          signIn()
+        },
+      })
+
+      if (status === "loading") {
+        return <Layout>
+            <div>Loading</div>
+        </Layout>
+      }
+
     return ( 
         <Layout>
-            <div>Profile Page</div>
+            <div>{session.user.name} Wellcome To Profile Page</div>
         </Layout>
      );
 }
